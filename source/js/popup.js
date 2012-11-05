546ab6a5258eab6b44ecbda8ec56e322e03db298
Megan O'Sullivan <megan.osullivan@gmail.com>
2012-11-04 16:50:40 -0800

    progress on checkbox

--- source/js/popup.js
+++ source/js/popup.js
@@ -29,8 +29,33 @@ function buttonClick(e) {
 	);
 }
 
-//addclickhandlers for buttons
+function changeHandler(e) {
+	chrome.browserAction.setBadgeText ( { text: "..." } );
+        chrome.tabs.executeScript(tabId,
+                {code:"purify = {'color':'"+e.target.id+"'}" },
+                function(){
+                	chrome.tabs.executeScript(tabId, {file: "js/highlightBlockquotes.js"},
+                        	function(){
+                                	chrome.tabs.executeScript(tabId,
+                                                {code:"purify.highlightBlockquotes()"},
+                                                function(){chrome.browserAction.setBadgeText ( { text: "done" } );
+                                                        setTimeout(function() {
+                                                                chrome.browserAction.setBadgeText ( { text: "" } );
+                                                        }, 1000);
+                                                }
+                                        );
+                                }
+                        );
+                }
+        );
+}
+
+//addclickhandlers for buttons & checkbox
 document.addEventListener('DOMContentLoaded', function () {
+	//button click
 	var btn = document.getElementById("highlight-btn");
 	btn.addEventListener('click', buttonClick);
+	
+	//on checkbox change
+	document.querySelector('#jump-quote').addEventListener('change', changeHandler);
 });
