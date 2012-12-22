049d620ee240c9c39fdedc15a2f1eef5c10a2993
adam-lynch <contact@adamlynch.ie>
2012-12-22 23:54:55 +0100

    latest

--- test/.travis-run-tests.sh
+++ test/.travis-run-tests.sh
@@ -4,4 +4,3 @@ sh server.sh start
 nohup java -jar "config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
 phantomjs config/phantomjs-jstd.js &
 java -jar "config/JsTestDriver.jar" --tests all --reset --config config/jsTestDriver.conf --testOutput results &
-exit
