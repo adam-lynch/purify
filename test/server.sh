3d8a8e76b997459fab47ea3f9f000d05e51f9b2b
Adam Lynch <contact@adamlynch.ie>
2013-01-09 21:42:40 +0100

    if format

--- test/server.sh
+++ test/server.sh
@@ -4,7 +4,6 @@ ROOTDIR="$( cd "$( dirname "$0")" && pwd )"
 JSTD_VERSION=1.3.4.b
 
 COMMAND=$1
-echo $COMMAND
 
 command -v phantomjs >/dev/null 2>&1 || { echo "Can't find phantomjs, please make sure it's on your PATH." >&2; exit 1; }
 
@@ -18,16 +17,17 @@ if [ ! -f "$ROOTDIR/coverage-$JSTD_VERSION.jar" ]; then
     curl http://js-test-driver.googlecode.com/files/coverage-$JSTD_VERSION.jar > $ROOTDIR/coverage-$JSTD_VERSION.jar
 fi
 
-if [[ $COMMAND == "start" ]]; then
-    echo "Starting JSTD Server"
+if [ $COMMAND == "start" ]
+    then
+		echo "Starting JSTD Server"
 
-    nohup java -jar $ROOTDIR/JsTestDriver-$JSTD_VERSION.jar --port 9876 > $ROOTDIR/jstd.out 2> $ROOTDIR/jstd.err < /dev/null &
-    echo $! > $ROOTDIR/jstd.pid
+		nohup java -jar $ROOTDIR/JsTestDriver-$JSTD_VERSION.jar --port 9876 > $ROOTDIR/jstd.out 2> $ROOTDIR/jstd.err < /dev/null &
+		echo $! > $ROOTDIR/jstd.pid
 
-    echo "Starting PhantomJS"
+		echo "Starting PhantomJS"
 
-    nohup phantomjs phantomjs-jstd.js > $ROOTDIR/phantomjs.out 2> $ROOTDIR/phantomjs.err < /dev/null &
-    echo $! > $ROOTDIR/phantomjs.pid
+		nohup phantomjs phantomjs-jstd.js > $ROOTDIR/phantomjs.out 2> $ROOTDIR/phantomjs.err < /dev/null &
+		echo $! > $ROOTDIR/phantomjs.pid
 fi
 
 if [[ $COMMAND == "stop" ]]; then
