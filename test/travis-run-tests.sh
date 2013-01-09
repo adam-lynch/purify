6d87e16c9c8665b83bd183766bea073e59ecaf84
Adam Lynch <contact@adamlynch.ie>
2013-01-09 21:12:38 +0100

    absolute paths

--- test/travis-run-tests.sh
+++ test/travis-run-tests.sh
@@ -3,8 +3,8 @@ pwd
 ls -l
 chmod 777 ./test/server.sh
 chmod 777 ./test/config/JsTestDriver.jar
-sh server.sh stop > /dev/null
-sh server.sh start
-nohup java -jar "config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
+sh ./test/server.sh stop > /dev/null
+sh ./test/server.sh start
+nohup java -jar "./test/config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
 phantomjs config/phantomjs-jstd.js &
-java -jar "config/JsTestDriver.jar" --tests all --reset --config config/jsTestDriver.conf --testOutput results &
+java -jar "./test/config/JsTestDriver.jar" --tests all --reset --config config/jsTestDriver.conf --testOutput results &
