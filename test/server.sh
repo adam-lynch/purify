d200e5e014124065c25124d526475f2dd1bf3060
Adam Lynch <contact@adamlynch.ie>
2013-01-09 22:41:21 +0100

    double command

--- test/server.sh
+++ test/server.sh
@@ -16,3 +16,31 @@ if [ ! -f "$ROOTDIR/coverage-$JSTD_VERSION.jar" ]; then
     echo "Downloading coverage jar ..."
     curl http://js-test-driver.googlecode.com/files/coverage-$JSTD_VERSION.jar > $ROOTDIR/coverage-$JSTD_VERSION.jar
 fi
+
+if [ $COMMAND == $COMMAND ]; then
+	echo "Starting JSTD Server"
+
+	nohup java -jar $ROOTDIR/JsTestDriver-$JSTD_VERSION.jar --port 9876 > $ROOTDIR/jstd.out 2> $ROOTDIR/jstd.err < /dev/null &
+	echo $! > $ROOTDIR/jstd.pid
+
+	echo "Starting PhantomJS"
+
+	nohup phantomjs phantomjs-jstd.js > $ROOTDIR/phantomjs.out 2> $ROOTDIR/phantomjs.err < /dev/null &
+	echo $! > $ROOTDIR/phantomjs.pid
+fi
+
+if [ $COMMAND == "stop" ]; then
+    echo "Killing JSTD Server"
+
+    PID=`cat $ROOTDIR/jstd.pid`
+    kill $PID
+
+    rm -f $ROOTDIR/jstd.out $ROOTDIR/jstd.err $ROOTDIR/jstd.pid
+
+    echo "Killing PhantomJS"
+
+    PID=`cat $ROOTDIR/phantomjs.pid`
+    kill $PID
+
+    rm -f $ROOTDIR/phantomjs.out $ROOTDIR/phantomjs.err $ROOTDIR/phantomjs.pid
+fi
