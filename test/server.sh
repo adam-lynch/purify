5ccf8aa82a479cff4794634444d42a07ed66a510
Adam Lynch <contact@adamlynch.ie>
2013-01-09 23:01:35 +0100

    dos2unix

--- test/server.sh
+++ test/server.sh
@@ -8,7 +8,7 @@ START=stop
 
 command -v phantomjs >/dev/null 2>&1 || { echo "Can't find phantomjs, please make sure it's on your PATH." >&2; exit 1; }
 
-if [ ! -f "$ROOTDIR/JsTestDriver-$JSTD_VERSION.jar" ]; then
+if [ ! -f "$ROOTDIR/JsTestDriver-$JSTD_VERSION.jar" ]; then 
     echo "Downloading JsTestDriver jar ..."
     curl http://js-test-driver.googlecode.com/files/JsTestDriver-$JSTD_VERSION.jar > $ROOTDIR/JsTestDriver-$JSTD_VERSION.jar
 fi
