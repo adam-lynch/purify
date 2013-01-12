29dabdf1b2278d8fc852852c21ee302f4f20cdd0
Adam Lynch <contact@adamlynch.ie>
2013-01-12 20:27:09 +0100

    Using ANT instead of bash for server

--- test/travis-run-tests.sh
+++ test/travis-run-tests.sh
@@ -1,8 +1,7 @@
 
-chmod 777 ./test/server.sh
+chmod 777 ./test/server.xml
 chmod 777 ./test/config/JsTestDriver.jar
-sh ./test/server.sh monkey > /dev/null
-sh ./test/server.sh shark
+ant -f server.xml start
 nohup java -jar "./test/config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
 phantomjs config/phantomjs-jstd.js &
 java -jar "./test/config/JsTestDriver.jar" --tests all --reset --config config/jsTestDriver.conf --testOutput results &
