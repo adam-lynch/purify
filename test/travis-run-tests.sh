71c33c4928c916fe7d507d4c0ac2bd76ce8d67c5
Adam Lynch <contact@adamlynch.ie>
2013-01-12 22:09:34 +0100

    properties file permissions

--- test/travis-run-tests.sh
+++ test/travis-run-tests.sh
@@ -1,6 +1,7 @@
 
 chmod 777 ./test/server.xml
 chmod 777 ./test/ant-contrib-1.0b3.jar
+chmod 777 ./test/antcontrib.properties
 chmod 777 ./test/config/JsTestDriver.jar
 ant -f ./test/server.xml start
 nohup java -jar "./test/config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
