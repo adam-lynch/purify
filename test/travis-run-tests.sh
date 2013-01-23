ae7e236f9159ccdfe48ac917e3caa458ffbed8df
Adam Lynch <contact@adamlynch.ie>
2013-01-23 01:12:05 +0100

    Travis test with new jasmine HTML test runner

--- test/travis-run-tests.sh
+++
@@ -1,8 +0,0 @@
-
-chmod 777 ./test/server.xml
-chmod 777 ./test/ant-contrib-1.0b3.jar
-chmod 777 ./test/config/JsTestDriver.jar
-ant -f ./test/server.xml start
-nohup java -jar "./test/config/JsTestDriver.jar" --port 9876 --captureConsole > /dev/null &
-phantomjs config/phantomjs-jstd.js &
-java -jar "./test/config/JsTestDriver.jar" --tests all --reset --config config/jsTestDriver.conf --testOutput results &
